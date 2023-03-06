4e79988f0e707b810cc3025381b4dc09fec40e98
Shubham Kumar Singh <81859951+Shubh-k04@users.noreply.github.com>
2023-03-07 04:04:38 +0530

    Update aws_iam_account_public_role.tf

--- terraform/aws_iam_account_public_role.tf
+++ terraform/aws_iam_account_public_role.tf
@@ -2,215 +2,128 @@
 resource "aws_iam_role" "allowed" {
   name = "test_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": "*",
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": "*",
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  })
 }
-EOF
-}
-
 
 # Usecase handled by SNYK-CC-TF-117
 resource "aws_iam_role" "allowed_2" {
   name = "test_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
       },
-      "Effect": "Allow",
-      "Sid": ""
-    },
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": "*",
-      "Effect": "Allow",
-      "Sid": ""
-    }
-
-  ]
-}
-EOF
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": "*",
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "denied" {
   name = "test_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "AWS": "arn:aws:iam::123456789012:root"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "AWS": "arn:aws:iam::123456789012:root"
+        },
+        "Effect": "Deny",
+        "Sid": ""
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "denied_3" {
   name = "test_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "AWS": [
-          "arn:aws:iam::123456789012:root",
-          "123456789012"
-        ]
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "AWS": [
+            "arn:aws:iam::123456789012:root",
+            "123456789012"
+          ]
+        },
+        "Effect": "Deny",
+        "Sid": ""
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "denied_2" {
   name = "test_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "AWS": "123456789012"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "AWS": "123456789012"
+        },
+        "Effect": "Deny",
+        "Sid": ""
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "denied_4" {
   name = "test_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Deny",
+        "Sid": ""
       },
-      "Effect": "Allow",
-      "Sid": ""
-    },
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "AWS": "123456789012"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "AWS": "123456789012"
+        },
+        "Effect": "Deny",
+        "Sid": ""
+      }
+    ]
+  })
 }
 
 # Usecase handled by SNYK-CC-TF-117
-data "aws_iam_policy_document" "allowed" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-  }
-}
-
-
-# Usecase handled by SNYK-CC-TF-117
-data "aws_iam_policy_document" "allowed_2" {
-  statement {
-    actions = ["s3:create"]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-  }
-
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-  }
-}
-
-
-# Usecase handled by SNYK-CC-TF-117
-data "aws_iam_policy_document" "allowed_3" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "*"
-      identifiers = ["something", "*"]
-    }
-  }
-}
-
-
-data "aws_iam_policy_document" "denied" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "AWS"
-      identifiers = ["123456789012"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "denied_2" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::123456789012:root", "123456789012"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "denied_3" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::123456789012:arn:role/test", "aws:iam::123456789012:root", "123456789012"]
-    }
-  }
-}
-
 data "aws_iam_policy_document" "denied_4" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -223,7 +136,7 @@ data "aws_iam_policy_document" "denied_4" {
     actions = ["sts:AssumeRole"]
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::123456789012:arn:role/test", "aws:iam::123456789012:root", "123456789012"]
+      identifiers = ["123456789012"]
     }
   }
-}
+} 
