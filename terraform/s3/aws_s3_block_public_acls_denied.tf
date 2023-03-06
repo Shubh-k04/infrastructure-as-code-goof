0e32d89844d5f7630b2c4a0a96c570f43d3bca8b
Shubham Kumar Singh <81859951+Shubh-k04@users.noreply.github.com>
2023-03-07 03:55:02 +0530

    Update aws_s3_block_public_acls_denied.tf

--- terraform/s3/aws_s3_block_public_acls_denied.tf
+++ terraform/s3/aws_s3_block_public_acls_denied.tf
@@ -9,5 +9,5 @@ resource "aws_s3_bucket_public_access_block" "denied_2" {
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = false
+  restrict_public_buckets = true
 } 
